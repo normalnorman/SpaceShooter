c97e0aa4e463088de38040d065728c89d18c0f78
Ron Norman <normalnorman@gmail.com>
2014-03-07 15:24:02 -0600

    first commit

---
+++ Assets/Done/Done_Scripts/Done_PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Done_Boundary 
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class Done_PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Done_Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	public Transform cachedTransform;
+	public Vector3 startingPoint;
+	public float _horizontalLimit = 2.5f, _verticalLimit = 2.5f, dragSpeed = 0.05f;
+	 
+	private float nextFire;
+
+	void Start()
+	{
+		cachedTransform = transform;
+
+		startingPoint = cachedTransform.position;
+	}
+	
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play ();
+		}
+
+		if (Input.touchCount > 0) {
+			Vector3 deltaPosition = Input.GetTouch(0).deltaPosition;
+			Debug.Log ("object touched", null);
+			switch (Input.GetTouch(0).phase) 
+			{
+				case TouchPhase.Began:
+					break;
+				case TouchPhase.Moved:
+					DragObject(deltaPosition);
+					break;
+				case TouchPhase.Ended:
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement * speed;
+		
+		rigidbody.position = new Vector3
+		(
+			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);
+		
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+
+	void DragObject (Vector3 deltaPosition)
+	{
+		Debug.Log ("object dragged", null);
+		cachedTransform.position = new Vector3 
+		(
+				Mathf.Clamp ((deltaPosition.x * dragSpeed) + cachedTransform.position.x, startingPoint.x - boundary.xMax, startingPoint.x + boundary.xMax),
+				Mathf.Clamp ((deltaPosition.y * dragSpeed) + cachedTransform.position.y, startingPoint.y - boundary.zMax, startingPoint.y + boundary.zMax),
+				Mathf.Clamp ((deltaPosition.z * dragSpeed) + cachedTransform.position.z, startingPoint.z - boundary.zMax, startingPoint.z + boundary.zMax)
+		);
+
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+		/*Vector3 movement = new Vector3 (deltaPosition.x, 0.0f, deltaPosition.y);
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3 (deltaPosition.x*/
+		/*float moveHorizontal = deltaPosition.x - startingPoint.x;//Input.GetAxis ("Horizontal");
+		float moveVertical = deltaPosition.z - startingPoint.z;//Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement * speed;
+		
+		rigidbody.position = new Vector3
+			(
+				Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+				);
+		
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);*/
+	}
+
+}
---
+++ SpaceShooterIOS/Apphance-Pre-Production.framework/README.txt
@@ -0,0 +1,25 @@
+OVERVIEW
+--------
+IBM Mobile Quality Assurance Pre-Production Library
+Platform: iOS
+Version: 1.8.20
+
+ABOUT IBM MOBILE QUALITY ASSURANCE
+----------------------------------
+IBM Mobile Quality Assurance enables mobile developers to know precisely how their apps are working in the test lab and in the wild. A single library enables over-the-air app distribution, crash reports, in-app bug reports and user feedback. Now developers can discover in real time precisely what’s working – and what’s not – with their iOS, Android, and Windows Phone apps. Learn more by visiting: 
+
+http://www.quality4mobile.com
+
+INSTALLATION
+------------
+Installation instructions for this library can be found by visiting:
+
+http://www.quality4mobilehelp.com/library-installation/ios/
+
+GETTING MORE HELP
+-----------------
+For more information about using IBM Mobile Quality Assurance, visit our online help topics: 
+
+http://www.quality4mobilehelp.com
+
+If you have additional questions, email us at: support@quality4mobile.com
